e333fe268e171be943571d29610e4739854ffeb6
David Art <david.madbox@gmail.com>
2014-08-09 14:22:25 +0200

    First commit

---
+++ www/js/client.js
@@ -0,0 +1,103 @@
+var ws, autoconnect = false;
+var host = "192.168.0.4"; // The host is the name or IP address of the server.
+
+var canvas = {
+  width: 800,
+  height: 600,
+  ctx: null,
+
+  init: function() {
+    this.ctx = document.getElementById('canvasframe').getContext("2d");
+    this.ctx.canvas.width = this.width;
+    this.ctx.canvas.height = this.height;
+    this.ctx.lineCap = 'round';
+    this.setColor('black');
+  },
+  clear: function() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+  },
+  beginPath: function() {
+    this.ctx.beginPath();
+  },
+  lineTo: function(pos) {
+    this.ctx.lineTo(pos.x * this.width, pos.y * this.height);
+    this.ctx.stroke();
+  },
+  moveTo: function(pos) {
+    this.ctx.moveTo(pos.x * this.width, pos.y * this.height);
+  },
+  setColor: function(color) {
+    this.ctx.strokeStyle = color;
+  },
+  lineWidth: function(width) {
+    this.ctx.lineWidth = width;
+  }
+}
+
+window.onload = function() {
+  canvas.init();
+  $('#connect').click(function() { connect(); });
+  if (autoconnect) { connect(); }
+}
+
+function connect() {
+  ws = new WebSocket("ws://" + host + ":8888/client");
+  ws.onmessage = function(evt) {
+    var data = JSON.parse(evt.data);
+
+    for (var key in data) {
+
+      if (key == 'eval') {
+        eval(data[key]);
+
+      } else if (key == 'mode') {
+        if (data[key] == 'editor') {
+          $('#canvasframe').hide();
+          $('#mainframe').show();
+        } else if (data[key] == 'canvas') {
+          $('#mainframe').hide();
+          $('#canvasframe').show();
+        }
+
+      } else if (key == 'html') {
+        updateFrame(data[key])
+
+      } else if (key == 'canvas') {
+        cmd = data[key][0];
+        obj = data[key][1];
+        //~ console.log(cmd, obj);
+        canvas[cmd](obj);
+
+      } else {
+        // what to do ?
+      }
+
+    }
+  };
+
+  ws.onclose = function(evt) {
+    console.log("WebSocket close ..");
+    $('#connect-box').slideDown();
+    $('#connect-box').addClass('connect-error');
+    $('#status').text("can't establish a connection to the server");
+  };
+
+  ws.onopen = function(evt) {
+    console.log("WebSocket open ..")
+    $('#status').text("");
+    $('#connect-box').removeClass('connect-error');
+    $('#connect-box').slideUp();
+  };
+
+  ws.onerror = function(evt) {
+    console.log("WebSocket error ..")
+  };
+}
+
+function updateFrame(data) {
+  var previewFrame = document.getElementById('mainframe');
+  var preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
+  preview.open();
+  preview.write(data);
+  preview.close();
+}
---
+++ www/js/remote.js
@@ -0,0 +1,210 @@
+var ws = null, code_html, code_css, code_js;
+var width = 0, height = 0, preview = false, mode = "editor";
+var mouse = 0;
+
+var canvas = {
+  width: 800,
+  height: 600,
+  ctx: null,
+
+  init: function() {
+    this.ctx = document.getElementById('canvasframe').getContext("2d");
+    this.ctx.canvas.width = this.width;
+    this.ctx.canvas.height = this.height;
+    this.ctx.lineCap = 'round';
+    this.setColor('black');
+  },
+  clear: function() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    send({"canvas": ["clear", ""]});
+  },
+  beginPath: function() {
+    this.ctx.beginPath();
+    send({"canvas": ["beginPath", ""]});
+  },
+  lineTo: function(pos) {
+    this.ctx.lineTo(pos.x * this.width, pos.y * this.height);
+    this.ctx.stroke();
+    send({"canvas": ["lineTo", pos]});
+  },
+  moveTo: function(pos) {
+    this.ctx.moveTo(pos.x * this.width, pos.y * this.height);
+    send({"canvas": ["moveTo", pos]});
+  },
+  setColor: function(color) {
+    this.ctx.strokeStyle = color;
+    send({"canvas": ["setColor", color]});
+  },
+  lineWidth: function(width) {
+    this.ctx.lineWidth = width;
+    send({"canvas": ["lineWidth", width]});
+  }
+}
+
+
+window.onload = function() {
+  canvas.init();
+  resizeEditor();
+
+  $('#code_html').show();
+  $('#nav-canvas').hide();
+  $('#canvasframe').hide();
+
+  $(window).resize(resizeEditor);
+
+  $('#connect').click(function() { connect(); });
+  $('#canvas-clear').click(function() { canvas.clear(); });
+  $('#canvas-color').change(function() { canvas.setColor($(this).val()); });
+  $('#canvas-lineWidth').change(function() { canvas.lineWidth($(this).val()); });
+
+  $('#canvasframe').mousedown(function(evt) {
+    var offset = $(this).offset();
+    var x = (evt.clientX - offset.left) / width;
+    var y = (evt.clientY - offset.top) / height;
+    mouse = {x: x, y: y}
+    canvas.beginPath();
+    canvas.moveTo(mouse);
+  });
+
+  $('#canvasframe').mousemove(function(evt) {
+    if (mouse) {
+      var offset = $(this).offset();
+      var x = (evt.clientX - offset.left) / width;
+      var y = (evt.clientY - offset.top) / height;
+      mouse = {x: x, y: y}
+      canvas.lineTo(mouse);
+    }
+  });
+
+  $('#canvasframe').mouseup(function(evt) {
+    mouse = 0;
+  });
+
+  $('#nav-editor li').click(function() {
+    // attribute name is the key ..
+    str = $(this).attr('name');
+
+    if (str == "refresh") {
+      data = "<html><head>";
+      data += "<style>" + code_css.getValue() + "</style>";
+      data += "</head><body>";
+      data += code_html.getValue();
+      data += "<script>" + code_js.getValue() + "<\/script>";
+      data += "</body></html>";
+
+      // send to server if connected
+      if (ws) {
+        ws.send(JSON.stringify({"html": data}));
+      }
+      // update <iframe> with new HTML
+      updateFrame(data);
+
+    } else if (str == "connect") {
+      //
+      $('#connect-box').slideToggle();
+
+    } else if (str == "preview") {
+      //
+        togglePreview();
+        resizeEditor();
+
+    } else {
+      $('.code').hide();
+      $('#code_'+str).toggle();
+      $('.active').removeClass('active');
+      $('#nav-editor li[name='+str+']').addClass('active');
+    }
+  });
+
+  $('.select-mode li').click(function() {
+    // attribute name is the key ..
+    mode = $(this).attr('name');
+    if (mode == "editor") {
+      $('#canvasframe').hide();
+      $('#nav-canvas').hide();
+      $('#editor').show();
+      $('#nav-editor').show();
+      $('#mode').text('Editor');
+    } else if (mode == "canvas") {
+      $('#editor').hide();
+      $('#nav-editor').hide();
+      $('#canvasframe').show();
+      $('#nav-canvas').show();
+      $('#mode').text('Canvas');
+    }
+    send({mode: mode});
+
+  });
+
+  code_html = ace.edit("code_html");
+  code_html.setTheme("ace/theme/twilight");
+  code_html.getSession().setMode("ace/mode/html");
+
+  code_css = ace.edit("code_css");
+  code_css.setTheme("ace/theme/twilight");
+  code_css.getSession().setMode("ace/mode/css");
+
+  code_js = ace.edit("code_js");
+  code_js.setTheme("ace/theme/twilight");
+  code_js.getSession().setMode("ace/mode/javascript");
+}
+
+function updateFrame(data) {
+  var previewFrame = document.getElementById('mainframe');
+  var preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
+  preview.open();
+  preview.write(data);
+  preview.close();
+}
+
+function send(data) {
+  if (ws) {
+    ws.send(JSON.stringify(data));
+  }
+}
+
+function togglePreview() {
+  if (preview) {
+    $('#preview').hide();
+  } else {
+    $('#preview').show();
+  }
+  preview = !preview;
+}
+
+function connect() {
+  host = $("#host").val();
+  ws = new WebSocket("ws://" + host + ":8888/remote");
+
+  ws.onmessage = function(evt) {
+    console.log(evt.data);
+  };
+
+  ws.onclose = function(evt) {
+    console.log("WebSocket close ..");
+    $('#connect-box').slideDown();
+    $('#connect-box').addClass('connect-error');
+    $('#status').text("can't establish a connection to the server");
+  };
+
+  ws.onopen = function(evt) {
+    console.log("WebSocket open ..")
+    $('#status').text("");
+    $('#connect-box').removeClass('connect-error');
+    $('#connect-box').slideUp();
+  };
+}
+
+function resizeEditor() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  $('.code').height(height-96);
+  $('#preview').height(height-96);
+  if (preview) {
+    $('.code').width(width/2-8);
+    $('#preview').width(width/2-8);
+  } else {
+    $('.code').width(width-4);
+    //~ $('#preview').width(width/2-8);
+  }
+}
